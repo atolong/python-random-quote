f85757ae46a34f4048562668bae7dbfe2a00350a
atolong <85973803+atolong@users.noreply.github.com>
2021-06-16 17:20:34 +0800

    输出字符
    输出字符

--- get-quote.py
+++ get-quote.py
@@ -1,21 +1,35 @@
+#第一次任务，输出字符
 def main():
-   #print("Keep it logically awesome.")
+   print("Keep it logically awesome.")
    
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-  
-  f = open("quotes.txt")
-quotes = f.readlines()
+   f = open("quotes.txt")#在第一次任务里没意义的三行代码
+   quotes = f.readlines()
+   f.close()
 
+if __name__== "__main__":
+  main()
+
+#第二次任务，输出该文件某行句子
+def main():
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+
+  print(quotes)
+  print(quotes[0])
+if __name__== "__main__":
+  main()
+
+#第三次任务，输出该文件随机行的句子
 import random
 last=13
-rnd=random.randint(o,last)
-last=len(quotes)-1
-
-print(quotes[rnd])
-f.close()
-  print(quotes)
-
+rnd=random.randint(0,last)
+def main():
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+  last=len(quotes)-1
+  print(quotes[rnd])
+  
 if __name__== "__main__":
   main()
